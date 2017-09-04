de968134d4149ccd4202918ec47f41687b0b2706
seantdg <sm.davis@gmx.com>
2017-09-04 22:31:16 +0100

    Update init.js

--- currency-v1/test/integration/features/step_definitions/init.js
+++ currency-v1/test/integration/features/step_definitions/init.js
@@ -9,9 +9,7 @@ console.log('currency api: [' + config.currencyApi.domain + ', ' + config.curren
 module.exports = function() {
 	// cleanup before every scenario
 	this.Before(function(scenario, callback) {
-		this.apickli = new apickli.Apickli('https',
-										   config.currencyApi.domain + config.currencyApi.basepath,
-										   './test/integration/features/fixtures/');
+		this.apickli = new apickli.Apickli('https', 'seandavis-test.apigee.net/currency/v1' ,'./test/integration/features/fixtures/');
 		callback();
 	});
 };
